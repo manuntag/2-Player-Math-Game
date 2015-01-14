1e89d89546ca1404c963aca3d8d360e2c189f5d7
manuntag <manuntag@ualberta.ca>
2015-01-13 16:13:15 -0800

    Added the ability for players to enter in names. Players are now referred to as their entered name.

--- 2 Player Math Game/main.m
+++ 2 Player Math Game/main.m
@@ -8,6 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
     
@@ -15,61 +16,78 @@ int main(int argc, const char * argv[]) {
         int player1score = 0;
         int player2score = 0;
         
+        char Player1Name[20];
+        char Player2Name[20];
+        
         BOOL gameIsOn = YES;
+        
+        
+        NSLog(@"Player 1, Please Enter Your Name");
+        scanf("%s", Player1Name);
+       
+        
+        
+        NSLog(@"Player 2, Please Enter Your Name");
+        scanf("%s", Player2Name);
+        
         
         while (gameIsOn) {
         
+        // Player 1 question
+            
         int randomNumber1 = arc4random()%20;
         int randomNumber2 = arc4random()%20;
     
         int sumOfRandomNumber = randomNumber1 + randomNumber2;
         
-        NSLog(@"Player 1, What is %d + %d", randomNumber1, randomNumber2);
-        
+        NSLog(@" %s, What is %d + %d", Player1Name, randomNumber1, randomNumber2);
+//        waitOnCR();
         scanf("%d", &answer);
         
         if(sumOfRandomNumber == answer) {
             
             NSLog(@"Right!");
             player1score = player1score + 1;
-            NSLog(@"Player 1, your score is now %d", player1score);
+            NSLog(@"%s, your score is now %d",Player1Name,  player1score);
             
             if (player1score == 3) {
-                NSLog(@"Player 1, you won!");
+                NSLog(@"%s, you won!", Player1Name);
                 break;
             }
             
             
         }else{
             
-            NSLog(@"Sorry");
+            NSLog(@"Sorry, that is not the right answer");
             player1score = player1score -1;
             
         }
+            
+            // Player 2 Question
             
             int randomNumber11 = arc4random()%20;
             int randomNumber22 = arc4random()%20;
             
             int sumOfRandomNumber2 = randomNumber11 + randomNumber22;
             
-            NSLog(@"Player 2, What is %d + %d", randomNumber11, randomNumber22);
+            NSLog(@"%s, What is %d + %d", Player2Name, randomNumber11, randomNumber22);
             
             scanf("%d", &answer);
-            
+           
             if(sumOfRandomNumber2 == answer) {
                 
                 NSLog(@"Right!");
                 player2score = player2score + 1;
-                NSLog(@"Player 2, your score is now %d", player2score);
+                NSLog(@" %s, your score is now %d",Player2Name, player2score);
                 
-                if (player1score == 3) {
-                    NSLog(@"Player 1, you won!");
+                if (player2score == 3) {
+                    NSLog(@"%s, you won!", Player2Name);
                     break;
                 }
             
             }else{
                 
-                NSLog(@"Sorry");
+                NSLog(@"Sorry that is not the right answer");
                 player2score = player2score -1;
             
             }
